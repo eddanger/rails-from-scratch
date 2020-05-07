8d279e9bf6d9c6cc149bd5d0f75fd39e4bc129bb
Michael Wood <mike@michaelwood.com>
2020-05-07 16:02:51 -0700

    PurgeCSS - Keep production css small

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue',
+    './app/javascript/**/*.jsx',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
